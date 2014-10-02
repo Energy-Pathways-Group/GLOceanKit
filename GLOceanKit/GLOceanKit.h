60458628f733515ce54dedfc6c1912ffc7c5e9b8
Jeffrey J. Early <jeffrey@jeffreyearly.com>
2014-10-02 09:17:57 -0700

    Initial Commit

---
+++ GLOceanKit/GLOceanKit.h
@@ -0,0 +1,19 @@
+//
+//  GLOceanKit.h
+//  GLOceanKit
+//
+//  Created by Jeffrey J. Early on 10/2/14.
+//  Copyright (c) 2014 Jeffrey J. Early. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GLOceanKit.
+FOUNDATION_EXPORT double GLOceanKitVersionNumber;
+
+//! Project version string for GLOceanKit.
+FOUNDATION_EXPORT const unsigned char GLOceanKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GLOceanKit/PublicHeader.h>
+
+
