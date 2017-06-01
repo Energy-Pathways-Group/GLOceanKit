aecf66a52d2a58991c16a873b9b10a09f209b061
JeffreyEarly <jeffrey@earlyinnovations.com>
2017-06-01 07:46:33 -0700

    Adding new function for horizontal velocity spectrum

---
+++ Matlab/InternalWaves/GarrettMunkHorizontalVelocitySpectrum.m
@@ -0,0 +1,7 @@
+function [ S ] = GarrettMunkHorizontalVelocitySpectrum( omega, latitude, rho, z, depth )
+%GarrettMunkHorizontalVelocitySpectrum Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
