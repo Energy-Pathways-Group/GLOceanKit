91129106b84cd91100c6f3980ba9fce156517432
JeffreyEarly <jeffrey@jeffreyearly.com>
2018-03-14 16:19:37 -0700

    Starting to add the k-constant solution
    much trickier

--- Matlab/InternalModes/InternalModesExponentialStratification.m
+++ Matlab/InternalModes/InternalModesExponentialStratification.m
@@ -36,7 +36,8 @@ classdef InternalModesExponentialStratification < InternalModesBase
             self.rho_z = -(self.rho0*self.N0*self.N0/self.g)*exp(2*self.z/self.b);
             self.rho_zz = -(2*self.rho0*self.N0*self.N0/self.g/self.b)*exp(2*self.z/self.b);
             
-            
+            % omega/(omega*exp(-self.Lz/self.b)
+            % omega/(self.N0*exp(-self.Lz/self.b)
             self.om = @(omega,c) omega*self.b./c;
             self.s = @(z,c) self.N0*self.b*exp(z/self.b)./c;
             self.shouldTruncate = @(omega,c,zT) (abs(bessely(self.om(omega,c),self.s(zT,c))/bessely(self.om(omega,c),self.s(-self.Lz,c) ))>1e-7);
@@ -44,9 +45,66 @@ classdef InternalModesExponentialStratification < InternalModesBase
             fprintf('Using the analytical form for exponential stratification N0=%.7g and b=%d\n',self.N0,self.b);
         end
                 
-        function [F,G,h,omega] = ModesAtWavenumber(self, k )
-            error('Not yet implemented.');
+        function [F,G,h,omega] = ModesAtWavenumber(self, k )            
+            epsilon = self.f0/self.N0;
+            lambda = k*self.b;
+            
+            x_lf = @(j,lambda) (j-1/4)*pi + lambda*pi/2;
+            x_hf = @(j,lambda) lambda.*(1+0.5*(3*pi*(4*j-1)./(lambda*8*sqrt(2))).^(2/3));
+            
+            self.nInitialModes = 128;
+            x_lowerbound = @(lambda) (lambda < 2*(1-1/4)*1e-1).*x_lf(1,lambda) + (lambda >= 2*(1-1/4)*1e-1).*x_hf(1,lambda);
+            x_upperbound = @(lambda) (lambda < (nInitialModes-1/4)).*x_lf(nInitialModes*1.1,lambda) + (lambda >= (nInitialModes-1/4)).*x_hf(nInitialModes*5,lambda);
+            
+            bounds = [x_lowerbound(lambda) x_upperbound(lambda)];
+            r = FindRootsInRange(self, epsilon, lambda, bounds);
+            
+            while length(r) < self.nModes
+                % the roots get closer together
+                dr = r(end)-r(end-1);
+                bounds = [bounds(2) bounds(2)+dr*self.nInitialModes];
+                more_roots = FindRootsInRange(self, epsilon, lambda, bounds);
+                r = [r; more_roots];
+            end
+            
+            h = reshape((self.b*eta./r).^2/self.g,1,[]);
+            h = h(1:self.nModes);
+            
             omega = self.omegaFromK(h,k);
+        end
+        
+        function r = FindRootsInRange(self, epsilon, lambda, bounds)
+            x = linspace(bounds(1),bounds(2),self.nInitialModes); % the choice of nInitialModes is somewhat arbitrary here.
+            
+            omega = @(x) sqrt( epsilon^2 * x.^2 + lambda^2 );
+            f_smallnu = @(x) besselj(omega(x),exp(-self.Lz/self.b)*x).*bessely(omega(x),x) - besselj(omega(x),x).*bessely(omega(x),exp(-self.Lz/self.b)*x);
+            f_bignu = @(x) besselj(omega(x),exp(-self.Lz/self.b)*x).*bessely(omega(x),x)./bessely(omega(x),exp(-self.Lz/self.b)*x) - besselj(omega(x),x);
+            
+            % The function omega(x)./(exp(-D/b)*x) will monotonically decay with x.
+            % We want to find where it first drops below 5, and use the appropriate
+            % form of the equation.
+            xcutoffIndex = find( omega(x)./(exp(-self.Lz/self.b)*x) < 5,1,'first');
+            
+            if xcutoffIndex == 1
+                % nu is small for all values of x
+                f = f_smallnu;
+                f_cheb = chebfun(f,bounds,'splitting','on');
+                r = roots(f_cheb);
+            elseif isempty(xcutoffIndex) == 1
+                % nu is large for all values of x
+                f = f_bignu;
+                f_cheb = chebfun(f,bounds,'splitting','on');
+                r = roots(f_cheb);
+            else
+                fprintf('big nu/small nu\n');
+                % we need to subdivide the interval into small and large.
+                f = f_bignu;
+                f_cheb = chebfun(f,[bounds(1) x(xcutoffIndex)] ,'splitting','on');
+                r = roots(f_cheb);
+                f = f_smallnu;
+                f_cheb = chebfun(f,[x(xcutoffIndex) bounds(2)] ,'splitting','on');
+                r = [r; roots(f_cheb)];
+            end
         end
         
         function [F,G,h,k] = ModesAtFrequency(self, omega )
@@ -123,12 +181,32 @@ classdef InternalModesExponentialStratification < InternalModesBase
             end
             omega_bar = @(c) self.om(omega,c);
             s_bar = self.s;
-            if self.shouldTruncate(omega,c,zT) == 0
+            %if self.shouldTruncate(omega,c,zT) == 0
+            if (omega/self.N0)*exp(self.Lz/self.b) > 5
+                fprintf('Using truncated solution.\n');
                 G = @(z,c) besselj( omega_bar(c),s_bar(z,c) )*bessely(omega_bar(c),s_bar(0,c));
                 F = @(z,c) (self.N0*exp(z/self.b)*c/2/self.g) .* ( (besselj(omega_bar(c)-1,s_bar(z,c)) - besselj(omega_bar(c) + 1,s_bar(z,c))) .*bessely(omega_bar(c),s_bar(0,c)) );
             else
+                fprintf('Using full solution.\n');
                 G = @(z,c) besselj( omega_bar(c),s_bar(z,c) )*bessely(omega_bar(c),s_bar(0,c)) - bessely(omega_bar(c),s_bar(z,c)) .* besselj(omega_bar(c),s_bar(0,c));
                 F = @(z,c) (self.N0*exp(z/self.b)*c/2/self.g) .* ( (besselj(omega_bar(c)-1,s_bar(z,c)) - besselj(omega_bar(c) + 1,s_bar(z,c))) .*bessely(omega_bar(c),s_bar(0,c)) - (bessely(omega_bar(c)-1,s_bar(z,c))-bessely(omega_bar(c)+1,s_bar(z,c))) .* besselj(omega_bar(c),s_bar(0,c)) );
+            end
+        end
+        
+        function G = GModeForOmegaAndC(self,omega,c)
+            % omega/(omega*exp(-self.Lz/self.b)
+            % omega/(self.N0*exp(-self.Lz/self.b)
+%             self.om = @(omega,c) omega*self.b./c;
+%             self.s = @(z,c) self.N0*self.b*exp(z/self.b)./c;
+% zT = max(self.b*log(omega/self.N0),-self.Lz);
+            nu = omega*self.b/c;
+            x_max = self.N0*self.b/c;
+            x_min = x_max*exp(-self.Lz/self.b);
+            x = max(x_min, nu);
+            if abs(bessely(nu,x)/bessely(nu,x_min )) > 1e-7
+                G = @(z,c) besselj(nu,x_max*exp(z/self.b) )*bessely(nu,x_max);
+            else
+                G = @(z,c) besselj(nu,x_max*exp(z/self.b) )*bessely(nu,x_max) - bessely(nu,x_max*exp(z/self.b)) .* besselj(nu,x_max);
             end
         end
         
