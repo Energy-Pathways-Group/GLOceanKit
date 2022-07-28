2ed99f772fce33ba15907c3af32981b2ddccdcb5
Jeffrey J. Early <JeffreyEarly@users.noreply.github.com>
2022-07-27 22:15:25 -0700

    Update WVDimensionAnnotation.m

--- Matlab/WaveVortexModel/WVDimensionAnnotation.m
+++ Matlab/WaveVortexModel/WVDimensionAnnotation.m
@@ -2,7 +2,7 @@ classdef WVDimensionAnnotation < WVAnnotation
     %Describes a coordinate dimension of the WVTransform
     %
     % In addition to adding a name, description and detailed description of
-    % a given dimension, you also specify tits  units. These annotations
+    % a given dimension, you also specify its units. These annotations
     % are used for both online documentation and for writing to NetCDF
     % files.
     %
