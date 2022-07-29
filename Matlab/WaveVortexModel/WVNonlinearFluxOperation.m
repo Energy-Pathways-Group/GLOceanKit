82a7f69d55eca60b841190396564f4b6ca5220f7
Jeffrey J. Early <JeffreyEarly@users.noreply.github.com>
2022-07-29 07:34:00 -0700

    Adding documentation for nonlinear flux

--- Matlab/WaveVortexModel/WVNonlinearFluxOperation.m
+++ Matlab/WaveVortexModel/WVNonlinearFluxOperation.m
@@ -1,6 +1,13 @@
 classdef WVNonlinearFluxOperation < WVOperation
     %Computes the nonlinear flux for a WVTransform
     %
+    % A WVNonlinearFluxOperation defines how energy moves between the
+    % wave-vortex coefficients---these are the nonlinear terms in the
+    % equations of motion, transformed into wave-vortex space. The most
+    % basic implementation is a freely evolving, unforced (and undamped)
+    % flux. Subclasses of WVNonlinearFluxOperation can implement custom
+    % forcing and damping.
+    %
     % The output variables *must* be at least one of {Fp,Fm,F0}, in that
     % order. The properties `doesFluxAp` etc. should be appropriately set
     % to match the output.
@@ -8,7 +15,8 @@ classdef WVNonlinearFluxOperation < WVOperation
     % You may also optionally output additional variables that are computed
     % as a by product of your flux calculation. Those variables will then
     % be cached, making other function calls much quicker.
-
+    %
+    % - Declaration: classdef WVNonlinearFluxOperation < WVOperation
     properties
         doesFluxAp = 1
         doesFluxAm = 1
