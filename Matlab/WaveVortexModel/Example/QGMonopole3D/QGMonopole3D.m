6e1df671d80123f59ea7a0ec3e89844d909e2483
Jeffrey J. Early <JeffreyEarly@users.noreply.github.com>
2023-05-16 14:59:06 -0700

    Updating 3D QG monopole

--- Matlab/WaveVortexModel/Example/QGMonopole3D/QGMonopole3D.m
+++ Matlab/WaveVortexModel/Example/QGMonopole3D/QGMonopole3D.m
@@ -32,20 +32,20 @@ wvt = WVTransformHydrostatic([Lx, Ly, Lz], [Nx, Ny, Nz], N2=N2,latitude=25);
 % "Deep eddy"
 % We are going to have its velocity vanish at the bottom, and only have its
 % density anomaly in the middle--hence the errorfunction.
-x0 = 3*Lx/4;
-y0 = Ly/2;
-Le = 80e3;
-z0 = -wvt.Lz/4;
-He = wvt.Lz/10;
-U = 0.25; % m/s
-psi = @(x,y,z) U*(Le/sqrt(2))*exp(1/2)*exp(-((x-x0)/Le).^2 -((y-y0)/Le).^2) .* (erf((z-z0)/He)+1)/2;
+% x0 = 3*Lx/4;
+% y0 = Ly/2;
+% Le = 80e3;
+% z0 = -wvt.Lz/4;
+% He = wvt.Lz/10;
+% U = 0.25; % m/s
+% psi = @(x,y,z) U*(Le/sqrt(2))*exp(1/2)*exp(-((x-x0)/Le).^2 -((y-y0)/Le).^2) .* (erf((z-z0)/He)+1)/2;
 
 % "Shallow eddy"
 % Density anomaly sits close to the surface
-% Le = 35e3;
-% He = wvt.Lz/5;
-% U = 0.25; % m/s
-% psi = @(x,y,z) U*(Le/sqrt(2))*exp(1/2)*exp(-((x-x0)/Le).^2 -((y-y0)/Le).^2 -(z/He).^2 );
+Le = 80e3;
+He = wvt.Lz/5;
+U = 0.30; % m/s
+psi = @(x,y,z) U*(Le/sqrt(2))*exp(1/2)*exp(-((x-x0)/Le).^2 -((y-y0)/Le).^2 -(z/He).^2 );
 
 wvt.setGeostrophicStreamfunction(psi);
 % fprintf('min-rv: %.2f f, max-rv: %.2f f\n',min(rv(:))/wvt.f,max(rv(:))/wvt.f)
@@ -54,9 +54,9 @@ wvt.setGeostrophicStreamfunction(psi);
 %% Plot SSH
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-p = wvt.p;
-ssh = p(:,:,end).'/(wvt.rho0*wvt.g);
-figure, pcolor(wvt.x/1e3, wvt.y/1e3, ssh), shading interp
+ssh = wvt.seaSurfaceHeight;
+figure, pcolor(wvt.x/1e3, wvt.y/1e3, ssh.'), shading interp
+max(ssh(:))
 
 % figure, pcolor(wvt.x,wvt.y,wvt.ssh.'), shading interp
 
@@ -67,7 +67,11 @@ figure, pcolor(wvt.x/1e3, wvt.y/1e3, ssh), shading interp
 % initialize the integrator with the model
 model = WVModel(wvt,nonlinearFlux=QGPVE(wvt,shouldUseBeta=1,u_damp=wvt.uMax));
 model.setupIntegrator(timeStepConstraint="advective",outputInterval=86400);
-model.integrateToTime(5*86400);
+model.createNetCDFFileForModelOutput('qg-eddy.nc')
+model.setNetCDFOutputVariables('u','v','eta','seaSurfaceHeight');
+model.integrateToTime(355*86400);
+
+return
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
