31a2025f85e4bb634f9000a47f2e0fc78f4510eb
Jeffrey J. Early <JeffreyEarly@users.noreply.github.com>
2024-02-04 15:23:41 -0800

    Made a simple unit test for nonlinear flux
    Could certainly be more robust, but for now unit testing revealed an error in bottom friction.

--- Matlab/WaveVortexModel/UnitTests/ProfileableSpeedTest.m
+++ Matlab/WaveVortexModel/UnitTests/ProfileableSpeedTest.m
@@ -11,9 +11,18 @@
 % April 12th, 2018      Version 1.0
 
 % wvt = WVTransformHydrostatic([15e3, 15e3, 5000], [64 64 33], N2=@(z) (5.2e-3)*(5.2e-3)*ones(size(z)));
-wvt = WVTransformBoussinesq([15e3, 15e3, 5000], [64 64 33], N2=@(z) (5.2e-3)*(5.2e-3)*ones(size(z)));
-% wvt = WVTransformConstantStratification([15e3, 15e3, 5000], [128, 128, 65]);
+% wvt = WVTransformBoussinesq([15e3, 15e3, 5000], [64 64 33], N2=@(z) (5.2e-3)*(5.2e-3)*ones(size(z)));
+wvt = WVTransformConstantStratification([15e3, 15e3, 5000], [8, 8, 5]);
 wvt.initWithRandomFlow();
+
+wvt.removeEnergyFromAliasedModes();
+spatialFlux = WVNonlinearFluxSpatial(wvt);
+standardFlux = WVNonlinearFlux(wvt,shouldAntialias=0);
+
+[SpatialFp,SpatialFm,SpatialF0] = spatialFlux.compute(wvt);
+[StandardFp,StandardFm,StandardF0] = standardFlux.compute(wvt);
+
+return
 
 [Fp,Fm,F0] = wvt.nonlinearFlux();
 % profile on
---
+++ Matlab/WaveVortexModel/UnitTests/TestNonlinearFlux.m
@@ -0,0 +1,43 @@
+classdef TestNonlinearFlux < matlab.unittest.TestCase
+    properties
+        wvt
+    end
+
+    properties (ClassSetupParameter)
+        Lxyz = struct('Lxyz',[15e3, 15e3, 4e3]);
+        Nxyz = struct('Nx8Ny8Nz5',[8 8 5]);
+        % transform = {'constant','hydrostatic','boussinesq'};
+        transform = {'constant'};
+    end
+
+    methods (TestClassSetup)
+        function classSetup(testCase,Lxyz,Nxyz,transform)
+            switch transform
+                case 'constant'
+                    testCase.wvt = WVTransformConstantStratification(Lxyz, Nxyz);
+                case 'hydrostatic'
+                    testCase.wvt = WVTransformHydrostatic(Lxyz, Nxyz, N2=@(z) (5.2e-3)*(5.2e-3)*ones(size(z)));
+                case 'boussinesq'
+                    testCase.wvt = WVTransformBoussinesq(Lxyz, Nxyz, N2=@(z) (5.2e-3)*(5.2e-3)*ones(size(z)));
+            end
+        end
+    end
+
+    methods (Test)
+        function testNonlinearFlux(self)
+            self.wvt.initWithRandomFlow();
+            self.wvt.removeEnergyFromAliasedModes();
+            spatialFlux = WVNonlinearFluxSpatial(self.wvt);
+            standardFlux = WVNonlinearFlux(self.wvt,shouldAntialias=0);
+
+            [SpatialFp,SpatialFm,SpatialF0] = spatialFlux.compute(self.wvt);
+            [StandardFp,StandardFm,StandardF0] = standardFlux.compute(self.wvt);
+
+            self.verifyEqual(StandardFp,SpatialFp, "AbsTol",1e-7,"RelTol",1e-7);
+            self.verifyEqual(StandardFm,SpatialFm, "AbsTol",1e-7,"RelTol",1e-7);
+            self.verifyEqual(StandardF0,SpatialF0, "AbsTol",1e-7,"RelTol",1e-7);
+        end
+
+    end
+
+end
