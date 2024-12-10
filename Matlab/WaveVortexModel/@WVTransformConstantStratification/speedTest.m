e2ae601d60dd642fad6f693206a4d1835dd298fb
Jeffrey J. Early <JeffreyEarly@users.noreply.github.com>
2024-12-10 10:02:17 -0800

    Fixing resolution changes in constant stratification

--- Matlab/WaveVortexModel/@WVTransformConstantStratification/speedTest.m
+++ Matlab/WaveVortexModel/@WVTransformConstantStratification/speedTest.m
@@ -9,11 +9,15 @@ function resultsTable = speedTest
 Nxy = [32 64 64 128 128 128 256 256 256].';
 Nz =  [32 32 64 32 64 128 32 64 128].'+1;
 nReps = [50 50 50 50 50 50 50 25 10].';
+
+Nxy = [256 256 256 512 512 512 1024 1024 1024].';
+Nz = [33 65 129 129 257 513 129 257 513].';
+nReps = [10 10 10 5 5 5 5 2 1].';
 measuredTime = zeros(size(Nxy));
 
 for iProfile=1:length(Nxy)
     wvt = WVTransformConstantStratification([15e3, 15e3, 1300], [Nxy(iProfile) Nxy(iProfile) Nz(iProfile)]);
-    wvt.initWithRandomFlow(uvMax=0.01);
+    % wvt.initWithRandomFlow(uvMax=0.01);
     [Fp,Fm,F0] = wvt.nonlinearFlux();
     tic
     for iRep=1:nReps(iProfile)
