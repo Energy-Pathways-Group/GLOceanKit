ce34531538c920f000f9099016d0cf3505c169e9
Jeffrey J. Early <JeffreyEarly@users.noreply.github.com>
2025-03-19 21:51:42 -0700

    diffZG and diffZF now pass unit tests for 4 derivatives

--- Matlab/WaveVortexModel/@WVStratificationVariable/diffZF.m
+++ Matlab/WaveVortexModel/@WVStratificationVariable/diffZF.m
@@ -5,18 +5,31 @@ arguments
     n (1,1)     double = 1
 end
 
-if n ~= 1
-    error('Not yet implemented')
-end
-
 u = permute(u,[3 1 2]); % keep adjacent in memory
 u = reshape(u,self.Nz,[]);
 
-D = self.QG0inv*(squeeze(self.Q0 ./ self.P0).*self.PF0);
+if n == 1
+    DzF = -(self.N2/self.g) .* self.QG0inv*(squeeze(self.Q0 ./ self.P0).*self.PF0);
+    D = DzF;
+elseif n == 2
+    DzF = -(self.N2/self.g) .* self.QG0inv*(squeeze(self.Q0 ./ self.P0).*self.PF0);
+    DzG = self.PF0inv*(squeeze(self.P0./(self.Q0 .* self.h_0)).*self.QG0);
+    D = DzG*DzF;
+elseif n == 3
+    DzF = -(self.N2/self.g) .* self.QG0inv*(squeeze(self.Q0 ./ self.P0).*self.PF0);
+    DzzG = -(self.N2/self.g) .* self.QG0inv*(squeeze(1./self.h_0).*self.QG0);
+    D = DzzG*DzF;
+elseif n == 4
+    DzF = -(self.N2/self.g) .* self.QG0inv*(squeeze(self.Q0 ./ self.P0).*self.PF0);
+    DzzG = -(self.N2/self.g) .* self.QG0inv*(squeeze(1./self.h_0).*self.QG0);
+    DzG = self.PF0inv*(squeeze(self.P0./(self.Q0 .* self.h_0)).*self.QG0);
+    D = DzG * DzzG * DzF;
+else
+    error('Not yet implemented')
+end
 du =  D*u;
 
 du = reshape(du,self.Nz,self.Nx,self.Ny);
 du = permute(du,[2 3 1]);
-du = - shiftdim((self.N2/self.g),-2) .* du;
 
 end
--- Matlab/WaveVortexModel/@WVStratificationVariable/diffZG.m
+++ Matlab/WaveVortexModel/@WVStratificationVariable/diffZG.m
@@ -5,14 +5,25 @@ arguments
     n (1,1)     double = 1
 end
 
-if n ~= 1
-    error('Not yet implemented')
-end
-
 u = permute(u,[3 1 2]); % keep adjacent in memory
 u = reshape(u,self.Nz,[]);
 
-D = self.PF0inv*(squeeze(self.P0./(self.Q0 .* self.h_0)).*self.QG0);
+if n == 1
+    DzG = self.PF0inv*(squeeze(self.P0./(self.Q0 .* self.h_0)).*self.QG0);
+    D = DzG;
+elseif n == 2
+    DzzG = -(self.N2/self.g) .* self.QG0inv*(squeeze(1./self.h_0).*self.QG0);
+    D = DzzG;
+elseif n == 3
+    DzzG = -(self.N2/self.g) .* self.QG0inv*(squeeze(1./self.h_0).*self.QG0);
+    DzG = self.PF0inv*(squeeze(self.P0./(self.Q0 .* self.h_0)).*self.QG0);
+    D = DzG * DzzG;
+elseif n == 4
+    DzzG = -(self.N2/self.g) .* self.QG0inv*(squeeze(1./self.h_0).*self.QG0);
+    D = DzzG * DzzG;
+else
+    error('Not yet implemented')
+end
 du =  D*u;
 
 du = reshape(du,self.Nz,self.Nx,self.Ny);
