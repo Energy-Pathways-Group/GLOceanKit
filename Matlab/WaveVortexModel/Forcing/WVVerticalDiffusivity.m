5acf6b09eef92cbdd24b971a31f7bb6ac0618d04
Jeffrey J. Early <JeffreyEarly@users.noreply.github.com>
2025-03-19 22:05:06 -0700

    Adding (untested) proper vertical diffusivity forcing

---
+++ Matlab/WaveVortexModel/Forcing/WVVerticalDiffusivity.m
@@ -0,0 +1,79 @@
+classdef WVVerticalDiffusivity < WVForcing
+    % Vertical diffusivty
+    %
+    %
+    % - Topic: Initializing
+    % - Declaration: WVVerticalDiffusivity < [WVForcing](/classes/forcing/wvforcing/)
+    properties
+        kappa_z
+        dLnN2 = 0
+    end
+
+    methods
+        function self = WVVerticalDiffusivity(wvt,options)
+            % initialize the WVNonlinearFlux nonlinear flux
+            %
+            % - Declaration: nlFlux = WVAdaptiveViscosity(wvt)
+            % - Parameter wvt: a WVTransform instance
+            % - Returns self: a WVAdaptiveViscosity instance
+            arguments
+                wvt WVTransform {mustBeNonempty}
+                options.kappa_z double = 1e-5
+            end
+            self@WVForcing(wvt,"vertical diffusivity",WVForcingType(["Spectral","PVSpectral"]));
+            self.wvt = wvt;
+            self.kappa_z = options.kappa_z;
+            if isa(wvt,'WVStratificationVariable')
+                self.dLnN2 = shiftdim(wvt.dLnN2,-2);
+            end
+        end
+
+        function [Fu, Fv, Feta] = addHydrostaticSpatialForcing(self, wvt, Fu, Fv, Feta)
+            Feta = Feta + self.kappa_z * (wvt.diffZG(wvt.eta,2) - self.dLnN2);
+        end
+
+        function [Fu, Fv, Fw, Feta] = addNonhydrostaticSpatialForcing(self, wvt, Fu, Fv, Fw, Feta)
+            Feta = Feta + self.kappa_z * (wvt.diffZG(wvt.eta,2) - self.dLnN2);
+        end
+
+        function Fpv = addPotentialVorticitySpatialForcing(self, wvt, Fpv)
+            Fpv = Fpv - wvt.f * self.kappa_z * (wvt.diffZG(wvt.eta,3) - wvt.diffZG(self.dLnN2));
+        end
+
+        function force = forcingWithResolutionOfTransform(self, wvtX2)
+            % Creates a forcing with the resolution of the transform
+            %
+            % - Declaration: forcingWithResolutionOfTransform(self, wvtX2)
+            % - Parameter self: an instance of WVAdaptiveViscosity
+            % - Parameter wvtX2: a WVTransform instance with doubled resolution
+            % - Returns: force
+            arguments
+                self WVVerticalDiffusivity {mustBeNonempty}
+                wvtX2 WVTransform {mustBeNonempty}
+            end
+            force = WVVerticalDiffusivity(wvtX2);
+        end
+    end
+    methods (Static)
+        function vars = classRequiredPropertyNames()
+            % Returns the required property names for the class
+            %
+            % - Declaration: classRequiredPropertyNames()
+            % - Returns: vars
+            arguments
+            end
+            vars = {};
+        end
+
+        function propertyAnnotations = classDefinedPropertyAnnotations()
+            % Returns the defined property annotations for the class
+            %
+            % - Declaration: classDefinedPropertyAnnotations()
+            % - Returns: propertyAnnotations
+            arguments (Output)
+                propertyAnnotations CAPropertyAnnotation
+            end
+            propertyAnnotations = CAPropertyAnnotation.empty(0,0);
+        end
+    end
+end
