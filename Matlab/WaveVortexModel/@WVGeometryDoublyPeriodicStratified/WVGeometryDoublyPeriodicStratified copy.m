67b68c4ee3096c4d667422ef5d87e5c76f369246
Jeffrey J. Early <JeffreyEarly@users.noreply.github.com>
2025-05-01 15:20:06 -0700

    Adding ClassHierarchy sketch.

--- Matlab/WaveVortexModel/@WVGeometryDoublyPeriodicStratified/WVGeometryDoublyPeriodicStratified copy.m
+++ Matlab/WaveVortexModel/@WVGeometryDoublyPeriodicStratified/WVGeometryDoublyPeriodicStratified copy.m
@@ -1,4 +1,4 @@
-classdef WVGeometryDoublyPeriodicStratified < WVGeometryDoublyPeriodic
+classdef WVGeometryDoublyPeriodicStratified < WVGeometryDoublyPeriodic & WVStratificationVariable
 
     properties (Dependent, SetAccess=private)
         spatialMatrixSize
@@ -8,13 +8,8 @@ classdef WVGeometryDoublyPeriodicStratified < WVGeometryDoublyPeriodic
         K, L, J
     end
 
-    properties (Abstract)
-        z, Nz
-        j, Nj
-    end
-
     methods
-        function self = WVGeometryDoublyPeriodicStratified(Lxy, Nxy, geomOptions)
+        function self = WVGeometryDoublyPeriodicStratified(Lxyz, Nxyz, geomOptions, stratOptions)
             % create geometry for 2D barotropic flow
             %
             % ```matlab
@@ -30,12 +25,40 @@ classdef WVGeometryDoublyPeriodicStratified < WVGeometryDoublyPeriodic
             % - Parameter shouldAntialias: (optional) whether or not to de-alias for quadratic multiplications
             % - Returns wvt: a new Cartesian2DBarotropic instance
             arguments
-                Lxy (1,2) double {mustBePositive}
-                Nxy (1,2) double {mustBePositive}
+                Lxyz (1,3) double {mustBePositive}
+                Nxyz (1,3) double {mustBePositive}
                 geomOptions.shouldAntialias (1,1) logical = true
+                stratOptions.z (:,1) double {mustBeNonempty} % quadrature points!
+                stratOptions.j (:,1) double {mustBeNonempty}
+                stratOptions.Nj (1,1) double {mustBePositive}
+                stratOptions.rhoFunction function_handle = @isempty
+                stratOptions.N2Function function_handle = @isempty
+                stratOptions.rho0 (1,1) double {mustBePositive} = 1025
+                stratOptions.planetaryRadius (1,1) double = 6.371e6
+                stratOptions.rotationRate (1,1) double = 7.2921E-5
+                stratOptions.latitude (1,1) double = 33
+                stratOptions.g (1,1) double = 9.81
+                stratOptions.dLnN2 (:,1) double
+                stratOptions.PF0inv
+                stratOptions.QG0inv
+                stratOptions.PF0
+                stratOptions.QG0
+                stratOptions.h_0 (:,1) double
+                stratOptions.P0 (:,1) double
+                stratOptions.Q0 (:,1) double
+                stratOptions.z_int (:,1) double
             end
             optionCell = namedargs2cell(geomOptions);
-            self@WVGeometryDoublyPeriodic(Lxy,Nxy,optionCell{:},Nz=Nxyz(3),shouldExcludeNyquist=true,shouldExludeConjugates=true,conjugateDimension=2);
+            self@WVGeometryDoublyPeriodic(Lxyz(1:2),Nxyz(1:2),optionCell{:},Nz=Nxyz(3),shouldExcludeNyquist=true,shouldExludeConjugates=true,conjugateDimension=2);
+
+            if geomOptions.shouldAntialias == true && ~isfield(stratOptions,"Nj")
+                maxNj = Nxyz(3)-1;
+                if maxNj > 3
+                    stratOptions.Nj = floor(2*maxNj/3);
+                end
+            end
+            statOptionCell = namedargs2cell(stratOptions);
+            self@WVStratificationVariable(Lxyz(3),Nxyz(3),statOptionCell{:});
         end
 
         function sz = get.spatialMatrixSize(self)
@@ -126,8 +149,9 @@ classdef WVGeometryDoublyPeriodicStratified < WVGeometryDoublyPeriodic
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
         function requiredPropertyNames = namesOfRequiredPropertiesForGeometry()
-            requiredPropertyNames = WVGeometryDoublyPeriodic.namesOfRequiredPropertiesForGeometry();
-            requiredPropertyNames = setdiff(requiredPropertyNames,WVGeometryDoublyPeriodicStratified.newNonrequiredPropertyNames);
+            requiredPropertyNames = WVStratificationVariable.namesOfRequiredPropertiesForStratification();
+            requiredPropertyNames = union(requiredPropertyNames,WVGeometryDoublyPeriodic.namesOfRequiredPropertiesForGeometry());
+            requiredPropertyNames = setdiff(requiredPropertyNames,WVGeometryDoublyPeriodicBarotropic.newNonrequiredPropertyNames);
         end
 
         function newNonrequiredPropertyNames = newNonrequiredPropertyNames()
@@ -136,6 +160,7 @@ classdef WVGeometryDoublyPeriodicStratified < WVGeometryDoublyPeriodic
 
         function propertyAnnotations = propertyAnnotationsForGeometry()
             propertyAnnotations = WVGeometryDoublyPeriodic.propertyAnnotationsForGeometry();
+            propertyAnnotations = cat(2,propertyAnnotations,WVStratificationVariable.propertyAnnotationsForStratification());
 
             propertyAnnotations(end+1) = CANumericProperty('K',{'j','kl'},'rad/m', 'k-coordinate matrix', detailedDescription='- topic: Domain Attributes — Grid — Spectral');
             propertyAnnotations(end+1) = CANumericProperty('L',{'j','kl'},'rad/m', 'l-coordinate matrix', detailedDescription='- topic: Domain Attributes — Grid — Spectral');
@@ -147,41 +172,41 @@ classdef WVGeometryDoublyPeriodicStratified < WVGeometryDoublyPeriodic
             propertyAnnotations(end+1) = CANumericProperty('Z',{'x','y','z'},'m', 'z-coordinate matrix', detailedDescription='- topic: Domain Attributes — Grid — Spatial');
         end
 
-        % function [Lxyz, Nxyz, options] = requiredPropertiesForGeometryFromGroup(group)
-        %     arguments (Input)
-        %         group NetCDFGroup {mustBeNonempty}
-        %     end
-        %     arguments (Output)
-        %         Lxyz (1,3) double {mustBePositive}
-        %         Nxyz (1,3) double {mustBePositive}
-        %         options
-        %     end
-        %     [Lxyz(1:2), Nxyz(1:2), geomOptions] = WVGeometryDoublyPeriodic.requiredPropertiesForGeometryFromGroup(group,shouldIgnoreMissingProperties=true);
-        %     [Lxyz(3), Nxyz(3), stratOptions] = WVStratificationVariable.requiredPropertiesForStratificationFromGroup(group);
-        %     options = cat(2,stratOptions,geomOptions);
-        % end
-        % 
-        % function geometry = geometryFromFile(path)
-        %     arguments (Input)
-        %         path char {mustBeNonempty}
-        %     end
-        %     arguments (Output)
-        %         geometry WVGeometryDoublyPeriodicStratified {mustBeNonempty}
-        %     end
-        %     ncfile = NetCDFFile(path);
-        %     geometry = WVGeometryDoublyPeriodicStratified.geometryFromGroup(ncfile);
-        % end
-        % 
-        % function geometry = geometryFromGroup(group)
-        %     arguments (Input)
-        %         group NetCDFGroup {mustBeNonempty}
-        %     end
-        %     arguments (Output)
-        %         geometry WVGeometryDoublyPeriodicStratified {mustBeNonempty}
-        %     end
-        %     [Lxyz, Nxyz, options] = WVGeometryDoublyPeriodicStratified.requiredPropertiesForGeometryFromGroup(group);
-        %     geometry = WVGeometryDoublyPeriodicStratified(Lxyz,Nxyz,options{:});
-        % end
+        function [Lxyz, Nxyz, options] = requiredPropertiesForGeometryFromGroup(group)
+            arguments (Input)
+                group NetCDFGroup {mustBeNonempty}
+            end
+            arguments (Output)
+                Lxyz (1,3) double {mustBePositive}
+                Nxyz (1,3) double {mustBePositive}
+                options
+            end
+            [Lxyz(1:2), Nxyz(1:2), geomOptions] = WVGeometryDoublyPeriodic.requiredPropertiesForGeometryFromGroup(group,shouldIgnoreMissingProperties=true);
+            [Lxyz(3), Nxyz(3), stratOptions] = WVStratificationVariable.requiredPropertiesForStratificationFromGroup(group);
+            options = cat(2,stratOptions,geomOptions);
+        end
+
+        function geometry = geometryFromFile(path)
+            arguments (Input)
+                path char {mustBeNonempty}
+            end
+            arguments (Output)
+                geometry WVGeometryDoublyPeriodicStratified {mustBeNonempty}
+            end
+            ncfile = NetCDFFile(path);
+            geometry = WVGeometryDoublyPeriodicStratified.geometryFromGroup(ncfile);
+        end
+
+        function geometry = geometryFromGroup(group)
+            arguments (Input)
+                group NetCDFGroup {mustBeNonempty}
+            end
+            arguments (Output)
+                geometry WVGeometryDoublyPeriodicStratified {mustBeNonempty}
+            end
+            [Lxyz, Nxyz, options] = WVGeometryDoublyPeriodicStratified.requiredPropertiesForGeometryFromGroup(group);
+            geometry = WVGeometryDoublyPeriodicStratified(Lxyz,Nxyz,options{:});
+        end
 
     end
 end
